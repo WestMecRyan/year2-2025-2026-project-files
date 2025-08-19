419ff80dd61c7189e8f6844cc8c043182d30b110
Ryan Morales <Ryan.Morales@west-mec.org>
2025-08-19 10:25:39 -0700

    built custom select

--- unit1/week4-express-and-APIs/Building-UI-functions/main.js
+++ unit1/week4-express-and-APIs/Building-UI-functions/main.js
@@ -2,9 +2,53 @@
 
 let mainContent = document.querySelector(".main-content");
 mainContent.innerHTML = "<h1>Hello</h1>";
-const aboveTag = document.createElement("div");
-aboveTag.innerHTML = "<h1>above</h1>";
-mainContent.prepend(aboveTag);
-// create an element to hold the below tag
-// and replace the append
-mainContent.append("<h1>below</h1>");
+
+function insertAboveAndBelow() {
+  const aboveTag = document.createElement("div");
+  aboveTag.innerHTML = "<h1>above</h1>";
+  mainContent.prepend(aboveTag);
+  const belowTag = document.createElement("div");
+  belowTag.innerHTML = "<h1>below</h1>";
+  mainContent.append(belowTag);
+}
+insertAboveAndBelow();
+
+const favoriteFoods = ["pizza", "burgers", "chicken"];
+
+const foodSelect = document.createElement("select");
+foodSelect.setAttribute("name", "choose-food");
+const disabledChoice = document.createElement("option");
+disabledChoice.setAttribute("selected", "");
+disabledChoice.setAttribute("disabled", "");
+disabledChoice.textContent = "choose one...";
+foodSelect.prepend(disabledChoice);
+
+favoriteFoods.forEach((food, index) => {
+  let thisFood = document.createElement("option");
+  thisFood.value = food;
+  thisFood.textContent = food.toUpperCase();
+  foodSelect.append(thisFood);
+});
+
+mainContent.append(foodSelect);
+
+// create a reusable function
+const favoriteCars = ["Bugatti", "Ferrari", "BMW"];
+function createOptionSelect(choiceArr, selectName) {
+  const customSelect = document.createElement("select");
+  customSelect.setAttribute("name", `${selectName}`);
+  const disabledChoice = document.createElement("option");
+  disabledChoice.setAttribute("selected", "");
+  disabledChoice.setAttribute("disabled", "");
+  disabledChoice.textContent = "choose one...";
+  customSelect.prepend(disabledChoice);
+  choiceArr.forEach((item, index) => {
+    let thisItem = document.createElement("option");
+    thisItem.value = item;
+    thisItem.textContent = item;
+    customSelect.append(thisItem);
+  });
+  return customSelect;
+}
+const carSelect = createOptionSelect(favoriteCars, "favorite-cars");
+mainContent.append(carSelect);
